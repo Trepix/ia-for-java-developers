a4acf924bb808b0681b5cc29cc433d20e9ef1c66
Trepix <pau.trepat.segura@gmail.com>
2022-05-31 01:59:46 +0200

    Translate add rule method

--- 1-SistemaExperto/src/main/java/io/trepix/ia/aplicacion/HumanMachineInterface.java
+++ 1-SistemaExperto/src/main/java/io/trepix/ia/aplicacion/HumanMachineInterface.java
@@ -21,17 +21,17 @@ public class HumanMachineInterface {
         
         // Agregar las reglas
         System.out.println("** Agregar las reglas **");
-        m.AgregarRegla("R1 : IF (Orden=3(¿Cuál es el orden?)) THEN  Triángulo");
-        m.AgregarRegla("R2 : IF (Triángulo AND Ángulo Recto(¿La figura tiene al menos un ángulo recto?)) THEN Triángulo Rectángulo");
-        m.AgregarRegla("R3 : IF (Triángulo AND Lados Iguales=2(¿Cuántos lados iguales tiene la figura?)) THEN Triángulo Isósceles");
-        m.AgregarRegla("R4 : IF (Triángulo rectángulo AND Triángulo Isósceles) THEN Triángulo Rectángulo Isósceles");
-        m.AgregarRegla("R5 : IF (Triángulo AND Lados Iguales=3(¿Cuántos lados iguales tiene la figura?)) THEN Triángulo Equilátero");
-        m.AgregarRegla("R6 : IF (Orden=4(¿Cuál es el orden?)) THEN Cuadrilátero");
-        m.AgregarRegla("R7 : IF (Cuadrilátero AND Lados Paralelos=2(¿Cuántos lados paralelos entre sí - 0, 2 o 4?)) THEN Trapecio");
-        m.AgregarRegla("R8 : IF (Cuadrilátero AND Lados Paralelos=4(¿Cuántos lados paralelos entre sí - 0, 2 o 4?)) THEN Paralelogramo");
-        m.AgregarRegla("R9 : IF (Paralelogramo AND Ángulo Recto(¿La figura tiene al menos un ángulo recto?)) THEN Rectángulo");
-        m.AgregarRegla("R10 : IF (Paralelogramo AND Lados Iguales=4(¿Cuántos lados iguales tiene la figura?)) THEN Rombo");
-        m.AgregarRegla("R11 : IF (Rectángulo AND Rombo THEN Cuadrado");
+        m.addRule("R1 : IF (Orden=3(¿Cuál es el orden?)) THEN  Triángulo");
+        m.addRule("R2 : IF (Triángulo AND Ángulo Recto(¿La figura tiene al menos un ángulo recto?)) THEN Triángulo Rectángulo");
+        m.addRule("R3 : IF (Triángulo AND Lados Iguales=2(¿Cuántos lados iguales tiene la figura?)) THEN Triángulo Isósceles");
+        m.addRule("R4 : IF (Triángulo rectángulo AND Triángulo Isósceles) THEN Triángulo Rectángulo Isósceles");
+        m.addRule("R5 : IF (Triángulo AND Lados Iguales=3(¿Cuántos lados iguales tiene la figura?)) THEN Triángulo Equilátero");
+        m.addRule("R6 : IF (Orden=4(¿Cuál es el orden?)) THEN Cuadrilátero");
+        m.addRule("R7 : IF (Cuadrilátero AND Lados Paralelos=2(¿Cuántos lados paralelos entre sí - 0, 2 o 4?)) THEN Trapecio");
+        m.addRule("R8 : IF (Cuadrilátero AND Lados Paralelos=4(¿Cuántos lados paralelos entre sí - 0, 2 o 4?)) THEN Paralelogramo");
+        m.addRule("R9 : IF (Paralelogramo AND Ángulo Recto(¿La figura tiene al menos un ángulo recto?)) THEN Rectángulo");
+        m.addRule("R10 : IF (Paralelogramo AND Lados Iguales=4(¿Cuántos lados iguales tiene la figura?)) THEN Rombo");
+        m.addRule("R11 : IF (Rectángulo AND Rombo THEN Cuadrado");
          
         // Resolución
         do {
--- 1-SistemaExperto/src/main/java/io/trepix/ia/sistemaexperto/InferenceEngine.java
+++ 1-SistemaExperto/src/main/java/io/trepix/ia/sistemaexperto/InferenceEngine.java
@@ -88,32 +88,33 @@ public class InferenceEngine {
         app.showFacts(knownFacts.getFacts());
     }
     
-    // Agregar una regla a la base a partir de su cadena
-    // En forma :
-    // Nombre : IF premisas THEN conclusion
-    public void AgregarRegla(String str) {
-        // Separación nombre:regla
-        String[] nombreRegla = str.split(":");
-        if (nombreRegla.length == 2) {
-            String nombre = nombreRegla[0].trim();
-            // Separación premisas THEN conclusión
-            String regla = nombreRegla[1].trim();
-            regla = regla.replaceFirst("^" + "IF", "");
-            String[] premisasConclusion = regla.split("THEN");
-            if (premisasConclusion.length == 2) {
-                // Lectura de las premises
-                ArrayList<Fact<?>> premises = new ArrayList<>();
-                String[] premisasStr = premisasConclusion[0].split(" AND ");
-                for(String cadena : premisasStr) {
-                    Fact<?> premisa = FactFactory.createFact(cadena.trim());
-                    premises.add(premisa);
-                }
-                // Lectura de la conclusión
-                String conclusionStr = premisasConclusion[1].trim();
-                Fact<?> conclusion = FactFactory.createFact(conclusionStr);
-                // Creación de la regla y adición a la base
-                rules.addRule(new Rule(nombre, premises, conclusion));
+    public void addRule(String input) {
+        String[] splitInput = input.split(":");
+        if (splitInput.length == 2) {
+            String name = splitInput[0].trim();
+            String rule = splitInput[1].trim();
+
+            rule = rule.replaceFirst("^IF", "");
+            String[] splitRule = rule.split("THEN");
+            if (splitRule.length == 2) {
+                String premisesAsString = splitRule[0];
+                String conclusionsAsString = splitRule[1];
+
+                ArrayList<Fact<?>> premises = createPremises(premisesAsString);
+                Fact<?> conclusion = FactFactory.createFact(conclusionsAsString);
+
+                rules.addRule(new Rule(name, premises, conclusion));
             }
         }
     }
+
+    private ArrayList<Fact<?>> createPremises(String premisesAsString) {
+        ArrayList<Fact<?>> premises = new ArrayList<>();
+        for(String premiseAsString : premisesAsString.split(" AND ")) {
+            Fact<?> premise = FactFactory.createFact(premiseAsString.trim());
+            premises.add(premise);
+        }
+        return premises;
+    }
+
 }
