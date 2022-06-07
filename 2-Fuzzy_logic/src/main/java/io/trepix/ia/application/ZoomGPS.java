ec219c0fda52db0b03d16f3df9e1de68d947a923
Trepix <pau.trepat.segura@gmail.com>
2022-06-07 22:23:45 +0200

    Translate main class to English  and also rules and Linguistic Variables

--- 2-Fuzzy_logic/src/main/java/io/trepix/ia/application/ZoomGPS.java
+++ 2-Fuzzy_logic/src/main/java/io/trepix/ia/application/ZoomGPS.java
@@ -8,64 +8,61 @@ import static io.trepix.ia.fuzzylogic.FuzzySetBuilder.BoundaryBuilder.startingAt
 import static io.trepix.ia.fuzzylogic.FuzzySetBuilder.*;
 import static io.trepix.ia.fuzzylogic.LinguisticValueBuilder.withName;
 
-// Clase principal, y sobrer el ejemplo del zoom de un GPS
 public class ZoomGPS {
     public static void main(String[] arg) {
-        System.out.println("Lógica difusa : caso del zoom de un GPS");
+        System.out.println("Fuzzy Logic : use case of zoom in a GPS");
 
-        // Creación del sistema
-        ControladorDifuso controlador = new ControladorDifuso("Gestión del zoom de un GPS");
+        ControladorDifuso controller = new ControladorDifuso("Management of the zoom of a GPS");
 
-        System.out.println("Agregar las variables de entrada");
-        LinguisticVariable distancia = LinguisticVariableBuilder
-                .withName("Distancia")
+        LinguisticVariable distance = LinguisticVariableBuilder
+                .withName("Distance")
                 .withMinimumValue(0)
                 .withMaximumValue(500000)
-                .andLinguisticValue(withName("Baja").andFuzzySet(
+                .andLinguisticValue(withName("Short").andFuzzySet(
                         OpenLeftTrapezoidal()
                                 .withBoundary(startingAt(30).endingAt(50)))
                 )
-                .andLinguisticValue(withName("Media").andFuzzySet(
+                .andLinguisticValue(withName("Middle").andFuzzySet(
                         ClosedTrapezoidal()
                                 .withLeftBoundary(startingAt(40).endingAt(50))
                                 .withRightBoundary(startingAt(100).endingAt(150)))
                 )
-                .andLinguisticValue(withName("Grande").andFuzzySet(
+                .andLinguisticValue(withName("Long").andFuzzySet(
                         OpenRightTrapezoidal()
                                 .withBoundary(startingAt(100).endingAt(150)))
                 ).build();
-        controlador.AgregarVariableEntrada(distancia);
+        controller.AgregarVariableEntrada(distance);
 
-        LinguisticVariable velocidad = LinguisticVariableBuilder
-                .withName("Velocidad")
+        LinguisticVariable speed = LinguisticVariableBuilder
+                .withName("Speed")
                 .withMinimumValue(0)
                 .withMaximumValue(200)
-                .andLinguisticValue(withName("Lenta").andFuzzySet(
+                .andLinguisticValue(withName("Slow").andFuzzySet(
                         OpenLeftTrapezoidal()
                                 .withBoundary(startingAt(20).endingAt(30)))
                 )
-                .andLinguisticValue(withName("PocoRapida").andFuzzySet(
+                .andLinguisticValue(withName("Steady").andFuzzySet(
                         ClosedTrapezoidal()
                                 .withLeftBoundary(startingAt(20).endingAt(30))
                                 .withRightBoundary(startingAt(70).endingAt(80)))
                 )
-                .andLinguisticValue(withName("Rapida").andFuzzySet(
+                .andLinguisticValue(withName("Fast").andFuzzySet(
                         ClosedTrapezoidal()
                                 .withLeftBoundary(startingAt(70).endingAt(80))
                                 .withRightBoundary(startingAt(90).endingAt(110)))
                 )
-                .andLinguisticValue(withName("MuyRapida").andFuzzySet(
+                .andLinguisticValue(withName("VeryFast").andFuzzySet(
                         OpenRightTrapezoidal()
                                 .withBoundary(startingAt(90).endingAt(110)))
                 ).build();
-        controlador.AgregarVariableEntrada(velocidad);
+        controller.AgregarVariableEntrada(speed);
 
-        System.out.println("Agregar la variable de salida");
+
         LinguisticVariable zoom = LinguisticVariableBuilder
                 .withName("Zoom")
                 .withMinimumValue(0)
                 .withMaximumValue(5)
-                .andLinguisticValue(withName("Pequeña").andFuzzySet(
+                .andLinguisticValue(withName("Small").andFuzzySet(
                         OpenLeftTrapezoidal()
                                 .withBoundary(startingAt(1).endingAt(2)))
                 )
@@ -74,62 +71,50 @@ public class ZoomGPS {
                                 .withLeftBoundary(startingAt(1).endingAt(2))
                                 .withRightBoundary(startingAt(3).endingAt(4)))
                 )
-                .andLinguisticValue(withName("Grande").andFuzzySet(
+                .andLinguisticValue(withName("Big").andFuzzySet(
                         OpenRightTrapezoidal()
                                 .withBoundary(startingAt(3).endingAt(4)))
                 ).build();
-        controlador.AgregarVariableSalida(zoom);
+        controller.AgregarVariableSalida(zoom);
 
-        System.out.println("Agregar las reglas");
-        // Agregar las diferentes reglas (9 para cubrir los 12 casos)
-        controlador.AgregarRegla("IF Distancia IS Grande THEN Zoom IS Pequeña");
-        controlador.AgregarRegla("IF Distancia IS Baja AND Velocidad IS Lenta THEN Zoom IS Normal");
-        controlador.AgregarRegla("IF Distancia IS Baja AND Velocidad IS PocoRapida THEN Zoom IS Normal");
-        controlador.AgregarRegla("IF Distancia IS Baja AND Velocidad IS Rapida THEN Zoom IS Grande");
-        controlador.AgregarRegla("IF Distancia IS Baja AND Velocidad IS MuyRapida THEN Zoom IS Grande");
-        controlador.AgregarRegla("IF Distancia IS Media AND Velocidad IS Lenta THEN Zoom IS Pequeña");
-        controlador.AgregarRegla("IF Distancia IS Media AND Velocidad IS PocoRapida THEN Zoom IS Normal");
-        controlador.AgregarRegla("IF Distancia IS Media AND Velocidad IS Rapida THEN Zoom IS Normal");
-        controlador.AgregarRegla("IF Distancia IS Media AND Velocidad IS MuyRapida THEN Zoom IS Grande");
 
-        System.out.println("Resolución de los caso prácticos");
-        // Caso práctico 1 : velocidad de 35 km/h, 
-        // y siguiente cambio de dirección a 70 m 
-        System.out.println("Cas 1 :");
-        controlador.AgregarValorNumerico(velocidad, 35);
-        controlador.AgregarValorNumerico(distancia, 70);
-        System.out.println("Resultado : " + controlador.Resolver() + "\n");
+        controller.AgregarRegla("IF Distance IS Long THEN Zoom IS Small");
+        controller.AgregarRegla("IF Distance IS Short AND Speed IS Slow THEN Zoom IS Normal");
+        controller.AgregarRegla("IF Distance IS Short AND Speed IS Steady THEN Zoom IS Normal");
+        controller.AgregarRegla("IF Distance IS Short AND Speed IS Fast THEN Zoom IS Big");
+        controller.AgregarRegla("IF Distance IS Short AND Speed IS VeryFast THEN Zoom IS Big");
+        controller.AgregarRegla("IF Distance IS Middle AND Speed IS Slow THEN Zoom IS Small");
+        controller.AgregarRegla("IF Distance IS Middle AND Speed IS Steady THEN Zoom IS Normal");
+        controller.AgregarRegla("IF Distance IS Middle AND Speed IS Fast THEN Zoom IS Normal");
+        controller.AgregarRegla("IF Distance IS Middle AND Speed IS VeryFast THEN Zoom IS Big");
 
-        // Caso práctico 2 : velocidad de 25 km/h, 
-        // y siguiente cambio de dirección a 70 m 
-        controlador.EliminarValoresNumericos();
-        System.out.println("Cas 2 :");
-        controlador.AgregarValorNumerico(velocidad, 25);
-        controlador.AgregarValorNumerico(distancia, 70);
-        System.out.println("Resultado : " + controlador.Resolver() + "\n");
+        System.out.println("Case 1 :");
+        controller.AgregarValorNumerico(speed, 35);
+        controller.AgregarValorNumerico(distance, 70);
+        System.out.println("Result : " + controller.Resolver() + "\n");
 
-        // Caso práctico 3 : velocidad de 72.5 km/h, 
-        // y siguiente cambio de dirección a 40 m 
-        controlador.EliminarValoresNumericos();
-        System.out.println("Cas 3 :");
-        controlador.AgregarValorNumerico(velocidad, 72.5);
-        controlador.AgregarValorNumerico(distancia, 40);
-        System.out.println("Resultado : " + controlador.Resolver() + "\n");
+        controller.EliminarValoresNumericos();
+        System.out.println("Case 2 :");
+        controller.AgregarValorNumerico(speed, 25);
+        controller.AgregarValorNumerico(distance, 70);
+        System.out.println("Result : " + controller.Resolver() + "\n");
 
-        // Caso práctico 4 : velocidad de 100 km/h, 
-        // y siguiente cambio de dirección a 110 m 
-        controlador.EliminarValoresNumericos();
-        System.out.println("Caso 4 :");
-        controlador.AgregarValorNumerico(velocidad, 100);
-        controlador.AgregarValorNumerico(distancia, 110);
-        System.out.println("Resultado : " + controlador.Resolver() + "\n");
+        controller.EliminarValoresNumericos();
+        System.out.println("Case 3 :");
+        controller.AgregarValorNumerico(speed, 72.5);
+        controller.AgregarValorNumerico(distance, 40);
+        System.out.println("Result : " + controller.Resolver() + "\n");
 
-        // Caso práctico 5 : velocidad de 45 km/h, 
-        // y cambio a 160 m 
-        controlador.EliminarValoresNumericos();
-        System.out.println("Caso 5 :");
-        controlador.AgregarValorNumerico(velocidad, 45);
-        controlador.AgregarValorNumerico(distancia, 160);
-        System.out.println("Resultado : " + controlador.Resolver() + "\n");
+        controller.EliminarValoresNumericos();
+        System.out.println("Case 4 :");
+        controller.AgregarValorNumerico(speed, 100);
+        controller.AgregarValorNumerico(distance, 110);
+        System.out.println("Result : " + controller.Resolver() + "\n");
+
+        controller.EliminarValoresNumericos();
+        System.out.println("Case 5 :");
+        controller.AgregarValorNumerico(speed, 45);
+        controller.AgregarValorNumerico(distance, 160);
+        System.out.println("Result : " + controller.Resolver() + "\n");
     }
 }
