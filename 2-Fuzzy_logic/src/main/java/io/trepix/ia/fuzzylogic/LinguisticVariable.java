fee33184686fd2b65051add1209b271709d6409e
Trepix <pau.trepat.segura@gmail.com>
2022-06-07 02:35:36 +0200

    Add equals function to Linguistic Variable to equal by value instead of reference

--- 2-Fuzzy_logic/src/main/java/io/trepix/ia/fuzzylogic/LinguisticVariable.java
+++ 2-Fuzzy_logic/src/main/java/io/trepix/ia/fuzzylogic/LinguisticVariable.java
@@ -2,6 +2,7 @@ package io.trepix.ia.fuzzylogic;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class LinguisticVariable {
     protected String name;
@@ -39,4 +40,16 @@ public class LinguisticVariable {
                 .orElseThrow();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinguisticVariable that = (LinguisticVariable) o;
+        return Objects.equals(name, that.name) && Objects.equals(linguisticValues, that.linguisticValues);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, linguisticValues);
+    }
 }
