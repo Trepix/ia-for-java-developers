60024aba3d0a7c7613aed85b3f442f792d3447df
Trepix <pau.trepat.segura@gmail.com>
2022-06-23 15:12:05 +0200

    Refactor and translate Bellman-Ford algorithm

--- 3-BusquedaRutas/src/main/java/io/trepix/ia/pathfinding/algorithms/BellmanFord.java
+++ 3-BusquedaRutas/src/main/java/io/trepix/ia/pathfinding/algorithms/BellmanFord.java
@@ -7,43 +7,37 @@ import io.trepix.ia.pathfinding.structure.Node;
 
 import java.util.List;
 
-// Algoritmo de Bellman-Ford
 public class BellmanFord<T extends Node<T>> extends PathFindingAlgorithm<T> {
-    // Constructor
+
     public BellmanFord() {
         super("Bellman-Ford");
     }
 
-    // Métodos de resolución
     @Override
     protected Path<T> execute(Graph<T> graph) {
-        // Iinicialización
-        boolean distanciaCambiada = true;
-        int i = 0;
-        List<Arc<T>> listaArcos = graph.arcs();
+        List<Arc<T>> arcs = graph.arcs();
 
-        // Bucle principal
-        int numBucleMax = graph.numberOfNodes() - 1;
-        while (i < numBucleMax && distanciaCambiada) {
-            distanciaCambiada = false;
-            for (Arc<T> arc : listaArcos) {
-                if (arc.origin().distanceFromStart() + arc.cost() < arc.destination().distanceFromStart()) {
-                    // Encontrado un camino más corto
-                    arc.destination().updateDistanceFromStart(arc.origin().distanceFromStart() + arc.cost());
-                    arc.destination().setParent(arc.origin());
-                    distanciaCambiada = true;
+        int maxNodesToVisit = graph.numberOfNodes() - 1;
+        boolean foundShortestPath = true;
+        for (int visitedNodes=0; visitedNodes < maxNodesToVisit && foundShortestPath; visitedNodes++) {
+            foundShortestPath = false;
+            for (Arc<T> arc : arcs) {
+                if (arc.isShorterThanKnownPathToDestination()) {
+                    arc.updatePathInfo();
+                    foundShortestPath = true;
                 }
             }
-            i++;
         }
-
-        // Prueba si el bucle es negativo
-        for (Arc<T> arc : listaArcos) {
-            if (arc.origin().distanceFromStart() + arc.cost() < arc.destination().distanceFromStart()) {
-                System.err.println("Bucle negativo - sin camino más corto");
-            }
-        }
+        checkIfThereAreNegativeLoops(arcs);
 
         return new Path<>(graph.pathSteps(), graph.endingNode().distanceFromStart());
+    }
+
+    private void checkIfThereAreNegativeLoops(List<Arc<T>> arcs) {
+        for (Arc<T> arc : arcs) {
+            if (arc.isShorterThanKnownPathToDestination()) {
+                System.err.println("Negative loop - There is no shorter way");
+            }
+        }
     }
 }
--- 3-BusquedaRutas/src/main/java/io/trepix/ia/pathfinding/structure/Arc.java
+++ 3-BusquedaRutas/src/main/java/io/trepix/ia/pathfinding/structure/Arc.java
@@ -1,3 +1,11 @@
 package io.trepix.ia.pathfinding.structure;
 
-public record Arc<T extends Node<T>>(T origin, T destination, double cost) { }
+public record Arc<T extends Node<T>>(T origin, T destination, double cost) {
+    public void updatePathInfo() {
+        this.destination().updatePathInfo(this.origin());
+    }
+
+    public boolean isShorterThanKnownPathToDestination() {
+        return this.origin().distanceFromStart() + this.cost() < this.destination().distanceFromStart();
+    }
+}
