55ad3ca767647f04976ef77d51269a3b2b5bf990
Trepix <pau.trepat.segura@gmail.com>
2022-07-27 01:32:17 +0200

    Migrate Tabu Search to new problem solving interface #2

--- 5-Metaheuristic/src/main/java/io/trepix/ia/Application.java
+++ 5-Metaheuristic/src/main/java/io/trepix/ia/Application.java
@@ -74,7 +74,7 @@ public class Application implements HumanMachineInterface {
 
     private static void runAlgorithm(Algorithm algorithm, Problem problem) {
         System.out.println(algorithm.name());
-        if (algorithm instanceof KnapsackGreedyAlgorithm) {
+        if (algorithm instanceof KnapsackGreedyAlgorithm || algorithm instanceof KnapsackTabuSearch) {
             Solution solution = algorithm._solve(problem);
             System.out.println(solution.toString());
         }
