b9c1d629041749f1724cefb40a52422a32719370
Trepix <pau.trepat.segura@gmail.com>
2022-07-28 01:09:57 +0200

    Translate some parts of Greedy Algorithm

--- 5-Metaheuristic/src/main/java/io/trepix/ia/knapsack/algorithms/KnapsackGreedyAlgorithm.java
+++ 5-Metaheuristic/src/main/java/io/trepix/ia/knapsack/algorithms/KnapsackGreedyAlgorithm.java
@@ -3,30 +3,26 @@ package io.trepix.ia.knapsack.algorithms;
 import io.trepix.ia.knapsack.Caja;
 import io.trepix.ia.knapsack.KnapsackProblem;
 import io.trepix.ia.knapsack.SolucionMochila;
+import io.trepix.ia.metaheuristics.Problem;
 import io.trepix.ia.metaheuristics.Solution;
-import io.trepix.ia.metaheuristics.algorithms.AlgorithmVoraz;
+import io.trepix.ia.metaheuristics.algorithms.GreedyAlgorithm;
 
 import java.util.ArrayList;
-import java.util.Collections;
-
-// Algoritmo voraz para el problema de la mochila
-public class KnapsackGreedyAlgorithm extends AlgorithmVoraz {
-    SolucionMochila solucion;
-
+public class KnapsackGreedyAlgorithm extends GreedyAlgorithm {
     @Override
-    protected Solution ConstruirSolucion() {
-        solucion = new SolucionMochila();
+    protected Solution findSolution(Problem problem) {
+        SolucionMochila solution = new SolucionMochila();
         KnapsackProblem pb = (KnapsackProblem) problem;
-        ArrayList<Caja> cajasPosibles = pb.Cajas();
-        Collections.sort(cajasPosibles, (Caja b1, Caja b2) -> (int) (((b2.valor/b2.pesos) >= (b1.valor/b1.pesos)) ? 1 : -1));
-        double espacioDispo = pb.pesosMax;
-        for (Caja b : cajasPosibles) {
-            if (b.pesos <= espacioDispo) {
-                solucion.contenido.add(b);
-                espacioDispo -= b.pesos;
+        ArrayList<Caja> items = pb.Cajas();
+        items.sort((Caja b1, Caja b2) -> ((b2.valor / b2.pesos) >= (b1.valor / b1.pesos)) ? 1 : -1);
+        double availableWeight = pb.pesosMax;
+        for (Caja b : items) {
+            if (b.pesos <= availableWeight) {
+                solution.contenido.add(b);
+                availableWeight -= b.pesos;
             }
         }
-        return solucion;
+        return solution;
     }
 
 }
