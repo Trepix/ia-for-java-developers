37659c1376c21da8e3249509d75e52a54545381a
Trepix <pau.trepat.segura@gmail.com>
2022-07-28 01:09:57 +0200

    Translate some parts of Greedy Algorithm

--- 5-Metaheuristic/src/main/java/io/trepix/ia/metaheuristics/algorithms/GreedyAlgorithm.java
+++ 5-Metaheuristic/src/main/java/io/trepix/ia/metaheuristics/algorithms/GreedyAlgorithm.java
@@ -3,21 +3,18 @@ package io.trepix.ia.metaheuristics.algorithms;
 import io.trepix.ia.metaheuristics.Algorithm;
 import io.trepix.ia.metaheuristics.Problem;
 import io.trepix.ia.metaheuristics.Solution;
-
-// Resolucion por algoritmo voraz : construcción progresiva de la solución
-public abstract class AlgorithmVoraz extends Algorithm {
+public abstract class GreedyAlgorithm extends Algorithm {
 
     protected Problem problem;
 
-    public AlgorithmVoraz() {
+    public GreedyAlgorithm() {
         super("Greedy Algorithm");
     }
 
     @Override
     public final Solution solve(Problem problem) {
-        this.problem = problem;
-        return ConstruirSolucion();
+        return findSolution(problem);
     }
 
-    protected abstract Solution ConstruirSolucion();
+    protected abstract Solution findSolution(Problem problem);
 }
