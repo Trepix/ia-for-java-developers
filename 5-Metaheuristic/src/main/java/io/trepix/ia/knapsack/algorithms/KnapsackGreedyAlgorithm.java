59834fc78eae111728192eac97d1914981cfbeb9
Trepix <pau.trepat.segura@gmail.com>
2022-07-28 01:43:46 +0200

    Remove unnecessary cast due to new generic interface

--- 5-Metaheuristic/src/main/java/io/trepix/ia/knapsack/algorithms/KnapsackGreedyAlgorithm.java
+++ 5-Metaheuristic/src/main/java/io/trepix/ia/knapsack/algorithms/KnapsackGreedyAlgorithm.java
@@ -3,19 +3,18 @@ package io.trepix.ia.knapsack.algorithms;
 import io.trepix.ia.knapsack.Caja;
 import io.trepix.ia.knapsack.KnapsackProblem;
 import io.trepix.ia.knapsack.SolucionMochila;
-import io.trepix.ia.metaheuristics.Problem;
 import io.trepix.ia.metaheuristics.Solution;
 import io.trepix.ia.metaheuristics.algorithms.GreedyAlgorithm;
 
 import java.util.ArrayList;
+
 public class KnapsackGreedyAlgorithm extends GreedyAlgorithm<KnapsackProblem> {
     @Override
-    protected Solution findSolution(Problem problem) {
+    protected Solution findSolution(KnapsackProblem problem) {
         SolucionMochila solution = new SolucionMochila();
-        KnapsackProblem pb = (KnapsackProblem) problem;
-        ArrayList<Caja> items = pb.Cajas();
+        ArrayList<Caja> items = problem.Cajas();
         items.sort((Caja b1, Caja b2) -> ((b2.valor / b2.pesos) >= (b1.valor / b1.pesos)) ? 1 : -1);
-        double availableWeight = pb.pesosMax;
+        double availableWeight = problem.pesosMax;
         for (Caja b : items) {
             if (b.pesos <= availableWeight) {
                 solution.contenido.add(b);
